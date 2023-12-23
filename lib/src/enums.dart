80d04de15e2b48db11970fbe39637c9da7ad8bb2
Vishal Kumar <vkgkrt@gmail.com>
2023-12-23 18:49:43 +0530

    Add json annotations and update dependencies in pubspec.yaml
    This commit adds json annotations to enum values in enums.dart for better serialization and readability. Moreover, dependencies 'json_annotation', 'build_runner' and 'json_serializable' have been added to pubspec.yaml to support these changes.

--- lib/src/enums.dart
+++ lib/src/enums.dart
@@ -1,19 +1,27 @@
+import 'package:json_annotation/json_annotation.dart';
+
 /// Enum representing different categories of harm in the system responsible for
 /// managing harmful prompts or candidates. Each value stands for a particular
 /// type of harmful content that may lead to the blocking of a prompt or
 /// candidate.
 enum HarmCategory {
+  @JsonValue("HARM_CATEGORY_UNSPECIFIED")
+
   /// Unspecified type of harm.
   unspecified,
+  @JsonValue("HARM_CATEGORY_HATE_SPEECH")
 
   /// Harm category involving hate speech.
   hateSpeech,
+  @JsonValue("HARM_CATEGORY_SEXUALLY_EXPLICIT")
 
   /// Harm category of sexually explicit content.
   sexuallyExplicit,
+  @JsonValue("HARM_CATEGORY_HARASSMENT")
 
   /// Harm category involving harassment.
   harassment,
+  @JsonValue("HARM_CATEGORY_DANGEROUS_CONTENT")
 
   /// Harm category of dangerous content.
   dangerousContent,
@@ -22,17 +30,23 @@ enum HarmCategory {
 /// Enum representing a threshold level at which a prompt or candidate might be
 /// blocked.
 enum HarmBlockThreshold {
+  @JsonValue("HARM_BLOCK_THRESHOLD_UNSPECIFIED")
+
   /// Threshold is unspecified.
   unspecified,
+  @JsonValue("BLOCK_LOW_AND_ABOVE")
 
   /// NEGLIGIBLE content will be allowed.
   blockLowAndAbove,
+  @JsonValue("BLOCK_MEDIUM_AND_ABOVE")
 
   /// NEGLIGIBLE and LOW content will be allowed.
   blockMediumAndAbove,
+  @JsonValue("BLOCK_ONLY_HIGH")
 
   /// NEGLIGIBLE, LOW, and MEDIUM content will be allowed.
   blockOnlyHigh,
+  @JsonValue("BLOCK_NONE")
 
   /// All content will be allowed.
   blockNone,
@@ -41,17 +55,23 @@ enum HarmBlockThreshold {
 /// Enum representing the probability of a prompt or candidate matching a
 /// harm category.
 enum HarmProbability {
+  @JsonValue("HARM_PROBABILITY_UNSPECIFIED")
+
   /// Probability is unspecified.
-  harmProbabilityUnspecified,
+  unspecified,
+  @JsonValue("NEGLIGIBLE")
 
   /// Negligible chance of content being unsafe.
   negligible,
+  @JsonValue("LOW")
 
   /// Low chance of content being unsafe.
   low,
+  @JsonValue("MEDIUM")
 
   /// Medium chance of content being unsafe.
   medium,
+  @JsonValue("HIGH")
 
   /// High chance of content being unsafe.
   high,
@@ -59,11 +79,15 @@ enum HarmProbability {
 
 /// Enum representing the reason a prompt was blocked.
 enum BlockReason {
+  @JsonValue("BLOCKED_REASON_UNSPECIFIED")
+
   /// Blocked reason not specified.
   unspecified,
+  @JsonValue("SAFETY")
 
   /// Blocked due to safety settings.
   safety,
+  @JsonValue("OTHER")
 
   /// Blocked for an uncategorized reason.
   other,
@@ -71,20 +95,27 @@ enum BlockReason {
 
 /// Enum representing the reason a candidate execution finished.
 enum FinishReason {
+  @JsonValue("FINISH_REASON_UNSPECIFIED")
+
   /// Default value. This value is unused.
   unspecified,
+  @JsonValue("STOP")
 
   /// Natural stop point of the model or provided stop sequence.
   stop,
+  @JsonValue("MAX_TOKENS")
 
   /// Maximum number of tokens as per the request was reached.
   maxTokens,
+  @JsonValue("SAFETY")
 
   /// Candidate was flagged for safety reasons.
   safety,
+  @JsonValue("RECITATION")
 
   /// Candidate was flagged for recitation reasons.
   recitation,
+  @JsonValue("OTHER")
 
   /// Finish reason unknown.
   other,
@@ -94,20 +125,27 @@ enum FinishReason {
 /// embedding the content. The Enum [TaskType] has six different types of
 /// tasks:
 enum TaskType {
+  @JsonValue("TASK_TYPE_UNSPECIFIED")
+
   /// Task type is not specified.
   unspecified,
+  @JsonValue("RETRIEVAL_QUERY")
 
   /// Task involves retrieval based on query.
   retrievalQuery,
+  @JsonValue("RETRIEVAL_DOCUMENT")
 
   /// Task involves retrieval of documents.
   retrievalDocument,
+  @JsonValue("SEMANTIC_SIMILARITY")
 
   /// Task involves finding semantic similarities.
   semanticSimilarity,
+  @JsonValue("CLASSIFICATION")
 
   /// Task involves classification.
   classification,
+  @JsonValue("CLUSTERING")
 
   /// Task involves clustering.
   clustering,
