185dd4b7a7e7ff2f0d42b6d4cbaf453210597849
Vishal Kumar <vkgkrt@gmail.com>
2023-12-24 20:40:03 +0530

    Add documentation and update .gitignore
    The documentation for the `generative_ai_dart` library has been added to provide detailed information about its usage, input types, and output types. Additionally, the `doc/` directory has been added to the .gitignore file to avoid its accidental inclusion in version control.

--- lib/generative_ai_dart.dart
+++ lib/generative_ai_dart.dart
@@ -1,3 +1,30 @@
+/// The `generative_ai_dart` library.
+///
+/// This library is a powerful tool for interacting with a wide array of
+/// Google's GenAI models. It features broad input support, including Blob and
+/// String data types, which enables it to generate robust chat sessions.
+/// With this package, the output can be either a stream of content or a
+/// singular content entity.
+///
+/// ## Key Features
+/// - **Google GenAI Models Interoperability:** Seamless interaction with Google's GenAI models.
+/// - **Diverse Input Support:** Supports different types of inputs such as Blob, String, etc.
+/// - **Chat Session Generation:** Capable of spawning and managing chat sessions.
+/// - **Stream or Single Content Output:** Allows generation of a stream of content or singular instances of content.
+///
+/// ## Usage
+/// To interact with the Google GenAI models, import the library and create a model object:
+///
+/// ```dart
+/// import 'package:generative_ai_dart/generative_ai_dart.dart';
+///
+/// void main() {
+///   final genAI = GenerativeModel();
+///   genAI.startChat();
+/// }
+/// ```
+///
+/// For more information on how to use this library, refer to the complete documentation.
 library generative_ai_dart;
 
 export 'src/generative_ai_dart_base.dart';
