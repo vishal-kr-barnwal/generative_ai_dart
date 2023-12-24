5079e99b05c07a551b42d5b395a30146c5bdbdd6
Vishal Kumar <vkgkrt@gmail.com>
2023-12-24 16:51:49 +0530

    Refactor content handling classes for simplicity
    Reworked the content management structure in generative AI implementations by removing unused classes (InputContent and StartChatParams). The 'role' concept was introduced into the Content class through two factory constructors (Content.user and Content.model), simplifying different role handling.

--- lib/src/types/content.dart
+++ lib/src/types/content.dart
@@ -1,159 +1,134 @@
-/// A [GenerativeContentBlob] object represents a content blob with
-/// a specific MIME type and data.
-///
+/// This class represents a content blob with a specific MIME type and data.
 /// Each blob is characterized by its MIME type (like `"text/plain"`,
 /// `"image/png"`, etc.) and the actual content in the form of a string.
-///
-/// This class has a factory constructor that constructs a `GenerativeContentBlob`
-/// object from a JSON object. It also has a method `toJson` that converts
-/// a [GenerativeContentBlob] object into a JSON object.
-final class GenerativeContentBlob {
-  /// The MIME type of the blob.
+/// This class has a factory constructor [GenerativeContentBlob.fromJson] that
+/// constructs a `GenerativeContentBlob` object from a JSON object. It also has
+/// a method [toJson] that converts a [GenerativeContentBlob] object into a JSON object.
+class GenerativeContentBlob {
+  /// A final string representing the MIME type of the blob
   final String mimeType;
 
-  /// The actual content of the blob.
+  /// A final string representing the actual content of the blob
   final String data;
 
-  /// Constructs a new `GenerativeContentBlob` object.
-  ///
-  /// Both the MIME type and the data must be provided.
+  /// Constructs a new [GenerativeContentBlob] object with MIME type and data
   GenerativeContentBlob({required this.mimeType, required this.data});
 
-  /// Constructs a `GenerativeContentBlob` from a JSON object.
-  ///
+  /// Factory method that returns a [GenerativeContentBlob] object from a JSON object.
   /// The JSON object must have `"mimeType"` and `"data"` keys.
-  factory GenerativeContentBlob.fromJson(final Map<String, dynamic> json) =>
-      GenerativeContentBlob(
-          mimeType: json["mimeType"] as String, data: json["data"] as String);
+  factory GenerativeContentBlob.fromJson(Map<String, dynamic> json) =>
+      GenerativeContentBlob(mimeType: json["mimeType"], data: json["data"]);
 
-  /// Converts the `GenerativeContentBlob` object into a JSON object.
+  /// Converts an instance of [GenerativeContentBlob] into a JSON object.
   Map<String, dynamic> toJson() => {'mimeType': mimeType, 'data': data};
 }
 
-/// The abstract base class [Part].
+/// An abstract base class named [Part].
 ///
-/// This class is the base for other classes and contains some common properties
-/// like [Part.text] and [Part.inlineData] and methods such as [Part.fromJson]
-/// and [Part.toJson] used in its derived classes.
+/// [Part] is a base class for other classes, containing common properties
+/// like [text] and [inlineData] and methods like [Part.fromJson] and
+/// [Part.toJson].
 abstract class Part {
-  /// An optional string that describes the part.
-  ///
-  /// It is handled in the [fromJson] factory constructor. Can be `null`.
+  /// An optional string that describes the part
   final String? text;
 
-  /// An optional [GenerativeContentBlob] that may be inline with part.
-  ///
-  /// It is handled in the [fromJson] factory constructor. Can be `null`.
+  /// An optional instance of [GenerativeContentBlob] that may be inline with part
   final GenerativeContentBlob? inlineData;
 
-  /// The private constructor.
-  ///
-  /// It is not meant to be used directly, instead use the factory constructor
-  /// [Part.fromJson].
+  /// A private constructor used internally in the class.
   Part._({this.text, this.inlineData});
 
-  /// Factory constructor for creating instances of [Part] from a JSON object.
+  /// Factory method for creating instances of [Part] from a JSON object.
   ///
-  /// This method will create either a `TextPart` or an `InlineDataPart` based on
-  /// provided [json]. If both `text` and `inlineData` in the [json] are
-  /// `null`, it will raise an AssertionError.
+  /// This method creates a variant of [Part] (`TextPart` or `InlineDataPart`)
+  /// based on the provided [json] object. If both `text` and `inlineData`
+  /// fields in the JSON are `null`, this method throws an `AssertionError`.
   factory Part.fromJson(Map<String, dynamic> json) {
     if (json["text"] != null) {
-      return TextPart(json["text"] as String);
+      return TextPart(json["text"]);
     }
     if (json["inlineData"] != null) {
-      return InlineDataPart(GenerativeContentBlob.fromJson(
-          json["inlineData"] as Map<String, dynamic>));
+      return InlineDataPart(GenerativeContentBlob.fromJson(json["inlineData"]));
     }
     throw AssertionError("Both Text and Inline Data can't be null");
   }
 
-  /// Method for serializing the [Part] object to a JSON map.
-  ///
-  /// Returns a [Map<String, dynamic>] which includes the [text] and
-  /// [inlineData].
+  /// A method to serialize a [Part] object to a JSON object.
   Map<String, dynamic> toJson() => {
         if (text != null) 'text': text,
         if (inlineData != null) 'inlineData': inlineData
       };
 }
 
-/// [TextPart] is a final class which extends from the [Part] class. It represents
-/// a section of text, with the text being derived from the super class.
-final class TextPart extends Part {
-  /// Overridden getter method to get the [text] from the super class.
-  /// It is assumed that the super class's [text] will not be null.
+/// This final class [TextPart] extends from the [Part] class. It represents a section of
+/// text, with the text being derived from the super class.
+class TextPart extends Part {
+  /// A getter to get the [text] from the base class, assumed not null
   @override
   String get text => super.text!;
 
-  /// Constructor for [TextPart] which takes a [String] as an input and calls the
-  /// constructor from the super class with the input text.
-  TextPart(final String text) : super._(text: text);
+  /// Constructor for [TextPart] that takes a string as an input and calls the
+  /// internal constructor from the base class with the input text
+  TextPart(String text) : super._(text: text);
 }
 
-/// [InlineDataPart] is a final class which extends from the [Part] class. It
-/// represents an inline data part, with the inline data being derived from
-/// the super class.
-final class InlineDataPart extends Part {
-  /// Overridden getter method to get the [inlineData] from the super class.
-  /// It is assumed that the super class's [inlineData] will not be null.
+/// This final class [InlineDataPart] extends from the [Part] class. It represents an inline
+/// data part, with the inline data being derived from the super class.
+class InlineDataPart extends Part {
+  /// A getter to get the [inlineData] from the base class, assumed not null
   @override
   GenerativeContentBlob get inlineData => super.inlineData!;
 
-  /// Constructor for [InlineDataPart] which takes a [GenerativeContentBlob] as
-  /// an input and calls the constructor from the super class with the input
+  /// Constructor for [InlineDataPart] that takes a [GenerativeContentBlob] as
+  /// an input and calls the internal constructor from the base class with the input
   /// inline data.
-  InlineDataPart(final GenerativeContentBlob inlineData)
+  InlineDataPart(GenerativeContentBlob inlineData)
       : super._(inlineData: inlineData);
 }
 
-/// [Content] is a final class which extends [InputContent].
-/// It overrides the [parts] getter to return a [List<Part]>] instead.
+/// This final class [Content] represents a list of [Part] instances.
 ///
-/// It has a constructor which requires a [List<Part>] and a [role],
-/// both of which are forwarded to the super constructor.
-///
-/// It has a factory method [Content.fromJson] which constructs a [Content]
-/// object from a JSON map. The [parts] field is extracted and
-/// each part is constructed using the [Part.fromJson] method.
-///
-/// The [toJson] method is overridden to convert the current
-/// [Content] object into a JSON map.
-final class Content {
+/// This class provides a constructor to create a [Content] object with a list
+/// of [Part], a role, and factory methods [Content.user] and [Content.model] to
+/// create an instance of [Content].
+class Content {
   final List<Part> parts;
-
   final String? role;
 
+  /// Construct a [Content] object;
   Content({required this.parts, required this.role});
 
-  /// Factory method to create a [Content] object from a JSON Map. It maps
-  /// each part in the parts list to [Part] object using [Part.fromJson]
+  /// Factory method to create a [Content] object from a JSON object that
+  /// contains a 'parts' field, where each part is constructed using
+  /// the [Part.fromJson] method.
+  ///
+  /// If 'parts' field is of type [String], then covert it to [TextPart]
+  /// and, add it to [parts]
   factory Content.fromJson(Map<String, dynamic> json) {
-    final parts = <Part>[];
-
     final partsJson = json["parts"];
+
+    final parts = <Part>[];
 
     if (partsJson is String) {
       parts.add(TextPart(partsJson));
     } else {
-      parts.addAll((partsJson as List)
-          .map((final part) => Part.fromJson(part as Map<String, dynamic>))
-          .toList());
+      partsJson?.forEach((partJson) {
+        parts.add(Part.fromJson(partJson));
+      });
     }
 
     return Content(parts: parts, role: json["role"] as String?);
   }
 
+  /// Factory method to create a user [Content] object.
   Content.user({required this.parts}) : role = "user";
 
+  /// Factory method to create a model [Content] object.
   Content.model({required this.parts}) : role = "model";
 
-  /// Convert the [Content] object into a JSON map. It maps each part
-  /// in the parts list to a raw map using [Part.toJson]
-  Map<String, dynamic> toJson() {
-    return {
-      "parts": parts.map((e) => e.toJson()).toList(),
-      if (role != null) "role": role
-    };
-  }
+  /// Converts the current [Content] object into a JSON object.
+  Map<String, dynamic> toJson() => {
+        "parts": parts.map((e) => e.toJson()).toList(),
+        if (role != null) "role": role
+      };
 }
