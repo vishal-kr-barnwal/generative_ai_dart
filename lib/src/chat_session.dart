b3971b971bc5c0db22ac8cca41f91093f4b82e20
Vishal Kumar <vkgkrt@gmail.com>
2023-12-24 19:47:10 +0530

    Refactor and document ChatSession class
    Refactored the 'ChatSession' class with added in-code documentation explaining the functionality every method featured in the code. In addition, a simplified code structure for updating chat history was implemented which improves readability and maintains similar functionality.

--- lib/src/chat_session.dart
+++ lib/src/chat_session.dart
@@ -1,26 +1,49 @@
+/// Dart libraries.
 import 'dart:async';
 
 import 'package:generative_ai_dart/generative_ai_dart.dart';
 
+/// `ChatSession` manages interaction with a [GenerativeModel].
+///
+/// It sends messages to and receives messages from an AI model.
 final class ChatSession {
+  /// An instance of [GenerativeModel] used for generating AI responses.
   final GenerativeModel _model;
+
+  /// [_history] stores the conversation history between the user and the AI.
   final List<Content> _history;
+
+  /// [_sendCompleter] used as a locking mechanism for asynchronous sending.
   var _sendCompleter = Completer()..complete();
 
+  /// Constructs a [ChatSession] with a provided [GenerativeModel].
+  ///
+  /// [history] is the optional parameter for pre-existing conversation history.
   ChatSession(
       {required GenerativeModel model, List<Content> history = const []})
       : _model = model,
         _history = [...history];
 
+  /// Fetch the chat history.
+  ///
+  /// Returns a [Future] that completes with the message history.
   Future<List<Content>> getHistory() =>
-      _sendCompleter.future.then((value) => _history);
+      _sendCompleter.future.then((_) => _history);
 
+  /// *Important:* This method is crucial for preserving the send-receive order.
+  ///
+  /// It ensures that a send operation is only possible once the previous
+  /// send operation has completed, thus avoiding overlaps.
   Future<void> _lockSend() async {
     await _sendCompleter.future;
 
     _sendCompleter = Completer();
   }
 
+  /// Sends a message to the AI model and waits for the response.
+  ///
+  /// Returns a [Future] that completes with the AI-generated content.
+  /// See also: [_lockSend]
   Future<GenerateContentResponse> sendMessage(List<Part> message) async {
     await _lockSend();
     final content = Content.user(message);
@@ -38,6 +61,11 @@ final class ChatSession {
     }
   }
 
+  /// Sends a message to the AI and gets the response in a [Stream].
+  ///
+  /// Yields [GenerateContentResponse] from the AI in a stream.
+  ///
+  /// See also: [_lockSend]
   Stream<GenerateContentResponse> sendMessageStream(List<Part> message) async* {
     await _lockSend();
     final content = Content.user(message);
@@ -55,6 +83,9 @@ final class ChatSession {
     }
   }
 
+  /// Update the chat history based on responses from the AI [Stream].
+  ///
+  /// Asynchronously waits for the response [Stream] and updates the chat history.
   Future<void> _updateHistoryFromStreamAndCompleteSend(
       Content input, Stream<GenerateContentResponse> output) async {
     try {
@@ -66,6 +97,9 @@ final class ChatSession {
     }
   }
 
+  /// Updates chat history and completes the [_sendCompleter].
+  ///
+  /// This function is called once the AI response is received.
   void _updateHistoryAndCompleteSend(
       Content input, List<GenerateContentResponse> output) {
     _history.add(input);
@@ -75,13 +109,8 @@ final class ChatSession {
         .expand((element) => element)
         .map((e) => e.content);
 
-    _history.addAll(outputContent.map((e) {
-      if (e.role == null) {
-        return Content.model(e.parts);
-      }
-
-      return e;
-    }));
+    _history.addAll(outputContent
+        .map((e) => (e.role == null) ? Content.model(e.parts) : e));
 
     _sendCompleter.complete();
   }
